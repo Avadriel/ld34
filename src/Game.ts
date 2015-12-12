c71f5b719b6bcd4cce0521cbd0a8f82c02da0b65
Yannik Daellenbach <yannikdbach@gmail.com>
2015-12-12 09:49:38 +0100

    use resource loader

--- src/Game.ts
+++ src/Game.ts
@@ -5,6 +5,7 @@ class Game {
 
 	canvas: HTMLCanvasElement;
 	gl: WebGLRenderingContext;
+	texture: WebGLTexture;
 
 	constructor() {
 		this.canvas = <HTMLCanvasElement>document.getElementById("stage");
@@ -19,5 +20,18 @@ class Game {
 
 		this.gl.clearColor(clearColor.r, clearColor.g, clearColor.b, clearColor.a);
 		this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+	}
+
+	start(){
+		console.log("Start");
+	}
+
+	loadTexture(resource){
+		this.texture = this.gl.createTexture();
+		this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture);
+		this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, resource);
+		this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
+		this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);
+		this.gl.bindTexture(this.gl.TEXTURE_2D, null);
 	}
 }
